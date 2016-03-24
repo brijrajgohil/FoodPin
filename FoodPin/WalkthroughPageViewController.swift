5d6941e791783e40c985c30b34bcc608337f43b6
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-03-24 11:55:53 +0530

    Add Walkthrough Screens

---
+++ FoodPin/WalkthroughPageViewController.swift
@@ -0,0 +1,73 @@
+//
+//  WalkthroughPageViewController.swift
+//  FoodPin
+//
+//  Created by Brijrajsinh Gohil on 24/03/16.
+//  Copyright © 2016 Brijrajsinh Gohil. All rights reserved.
+//
+
+import UIKit
+
+class WalkthroughPageViewController: UIPageViewController, UIPageViewControllerDataSource {
+
+    
+    var pageHeadings = ["Personalize", "Locate", "Discover"]
+    var pageImages = ["foodpin-intro-1", "foodpin-intro-2", "foodpin-intro-3"]
+    var pageContent = ["Pin your favourite restaurants and create your own food guide",
+    "Search and locate your favourite restaurant on Maps",
+    "Find restaurants pinned by your friends and other foodies around the world"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        dataSource = self
+        
+        let startingViewController = viewControllerAtIndex(0)
+        setViewControllers([startingViewController], direction: .Forward, animated: true, completion: nil)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        var index = (viewController as! WalkthroughContentViewController).index
+        index--
+        
+        return viewControllerAtIndex(index)
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        var index = (viewController as! WalkthroughContentViewController).index
+        index++
+        
+        return viewControllerAtIndex(index)
+    }
+
+    func viewControllerAtIndex(index: Int) -> WalkthroughContentViewController {
+        if index == NSNotFound || index < 0 || index >= pageHeadings.count {
+        }
+        
+        //Create a new view controller and pass suitable data
+        
+        if let pageContentViewController = storyboard?.instantiateViewControllerWithIdentifier("WalkthroughContentViewController") as? WalkthroughContentViewController {
+            pageContentViewController.imageFile = pageImages[index]
+            pageContentViewController.heading = pageHeadings[index]
+            pageContentViewController.content = pageContent[index]
+            pageContentViewController.index = index
+            return pageContentViewController
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
